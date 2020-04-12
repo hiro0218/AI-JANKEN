dfdc1ad6971c41c0a362bc1f38734fc123efc4eb
hiro0218 <hiro0218@gmail.com>
2020-04-13 02:17:41 +0900

    update: ogp

--- nuxt.config.js
+++ nuxt.config.js
@@ -10,6 +10,9 @@ export default {
    */
   head: {
     title: process.env.npm_package_name || '',
+    htmlAttrs: {
+      prefix: 'og: http://ogp.me/ns#'
+    },
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
@@ -17,6 +20,32 @@ export default {
         hid: 'description',
         name: 'description',
         content: process.env.npm_package_description || ''
+      },
+      {
+        hid: 'og:site_name',
+        property: 'og:site_name',
+        content: process.env.npm_package_name
+      },
+      { hid: 'og:type', property: 'og:type', content: 'website' },
+      {
+        hid: 'og:url',
+        property: 'og:url',
+        content: 'https://hiro0218.github.io/AI-JANKEN/'
+      },
+      {
+        hid: 'og:title',
+        property: 'og:title',
+        content: process.env.npm_package_name
+      },
+      {
+        hid: 'og:description',
+        property: 'og:description',
+        content: process.env.npm_package_description || ''
+      },
+      {
+        hid: 'og:image',
+        property: 'og:image',
+        content: 'https://hiro0218.github.io/AI-JANKEN/janken.png'
       }
     ],
     link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
